141a96e3552e860c932bb8095c86d575af19e7c7
Lucas Silva <silva.lucasdev@gmail.com>
2026-04-26 14:56:29 -0300

    feat(frontend) add NuxtUI

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,27 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="flex h-screen flex-col items-center justify-center">
-    <h1>You did it!</h1>
-    <p>
-      Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-      documentation
-    </p>
+  <div class="flex h-screen items-center justify-center">
+    <UApp>
+      <UCard variant="subtle" class="max-w-[20rem]">
+        <div class="flex flex-col items-start gap-6">
+          <div>
+            <h1>You did it!</h1>
+            <p class="text-muted mt-1 text-[12px]">
+              Visit
+              <a
+                href="https://vuejs.org/"
+                target="_blank"
+                rel="noopener"
+                class="text-primary underline"
+                >vuejs.org</a
+              >
+              to read the documentation. This is supposed to be some big text!
+            </p>
+          </div>
+          <UButton>Click me</UButton>
+        </div>
+      </UCard>
+    </UApp>
   </div>
 </template>
-
-<style scoped></style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,14 +1,15 @@
+import ui from "@nuxt/ui/vue-plugin";
 import { createPinia } from "pinia";
 import { createApp } from "vue";
 
+import "./style.css";
 import App from "./App.vue";
 import router from "./router";
-
-import "./style.css";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(ui);
 
 app.mount("#app");
